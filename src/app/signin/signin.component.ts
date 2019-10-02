e3804148b6596a2fe3a02429fa531e859f5a34fc
Matheus Barbosa <matheuskleber@live.com>
2019-10-02 10:33:24 -0300

    fix(component): Removendo codigo errado

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -12,23 +12,4 @@ export class SigninComponent implements OnInit {
   ngOnInit() {
   }
 
-  showTab(n: number){
-    // This function will display the specified tab of the form...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    //... and fix the Previous/Next buttons:
-    if (n == 0) {
-      document.getElementById("prevBtn").style.display = "none";
-    } else {
-      document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n == (x.length - 1)) {
-      document.getElementById("nextBtn").innerHTML = "Submit";
-    } else {
-      document.getElementById("nextBtn").innerHTML = "Next";
-    }
-    //... and run a function that will display the correct step indicator:
-    fixStepIndicator(n)
-  }
-
 }
