6b0461abba08f3ddd004a83f9d2819d6b22c91a2
Matheus Barbosa <matheuskleber@live.com>
2019-10-12 14:01:46 -0300

    fix(server): Mudando path dos arquivos necessarios para o server

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/stopots'));
+app.use(express.static(__dirname + '/dist/get-a-beer-front-end'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/stopots/index.html'));
+res.sendFile(path.join(__dirname+'/dist/get-a-beer-front-end/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
